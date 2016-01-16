939425b7bde463d01fe37ba7a10deb9d12da47b2
Chelsea Holbrook <cholbrook2@mail.valenciacollege.edu>
2016-01-16 00:17:39 -0500

    Aliens communicating

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,26 @@
-# replace the contents of this comment with your full name
+# Chelsea Holbrook
+
+print('Greetings, earthling!') #original greeting
+print('I am assuming you speak English. Correct me if I am wrong.') #engaging other options
+
+print('1. Spanish') #option 1
+print('2. Dutch') #option 2
+print('3. Swahili') #option 3
+
+language = input()
+if language == '1': #spanish scenario
+    print('Hola, terrícola')
+
+if language == '2': #dutch scenario
+    print('Hallo, aardbewoner!')
+
+if language == '3': #swahili scenario
+    print('Hodi wa udongo!')
 
 # write a program that:
 # 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
+# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!)
 # 3. displays the greeting in the chosen language
 # 4. exits
 
 # make sure that your code contains comments explaining your logic!
-
